d877f71fab9d3bc57f4fa3ca79ef0d0bde0630bc
jstevens00 <joeystevens00@gmail.com>
2017-01-13 20:45:06 -0500

    format

--- app/views.py
+++ app/views.py
@@ -3,26 +3,24 @@ from flask import render_template
 from flask import request
 from flask import flash
 import os
+from drcmsconfig import *
 @app.route('/')
 @app.route('/index',  methods=['GET', 'POST'])
 
 def index():
-
-    #Setup vars
-    dir='/home/zen/dev/web/asciidoctor-auto-run/adoc/'
-    #These don't need editing
-    f=""
-    req=""
-    #Displays the textarea
-    display=""
-    fileContent=""
-    filename=""
-    debug=""
-    #Value of the textarea
-    update=""
+    #########DEFAULT variables#######
+    adocdir='/home/zen/dev/web/asciidoctor-auto-run/adoc/'
+    f=''
+    req=''
+    display=''
+    fileContent=''
+    filename=''
+    debug=''
+    update=''
     emptyFileError=False
     successMessage=False
-    filelist = os.listdir(dir)	
+    #################################
+    filelist = os.listdir(adocdir)
     if request.method == 'POST':
        #If the save button was clicked
         if "Save" in request.form['submit']:
@@ -33,7 +31,7 @@ def index():
              #If we do have a filename 
              if filename != "":
                  update=request.form.get('text', "")
-                 wf = open(dir+filename, 'w')
+                 wf = open(adocdir+filename, 'w')
                  wf.write(update)
                  f=update
                  successMessage=True
@@ -43,10 +41,15 @@ def index():
         else:
             req=request.form['submit']
             display=True
-            f = open(dir+req)
+            f = open(adocdir+req)
             f=f.read()
             update=request.form
-    return render_template('index.html', title="Home", filelist=filelist, \
-    buttonclick=req, displayTextArea=display, fileContents=f, debug=debug, \
-    emptyFileError=emptyFileError, successMessage=successMessage)
+    return render_template('index.html', 
+    title="Home", 
+    filelist=filelist,
+    buttonclick=req,
+    displayTextArea=display,
+    fileContents=f, debug=debug,
+    emptyFileError=emptyFileError, 
+    successMessage=successMessage)
 
